6370d87e7f5be2795d3ff2bb1622a44077786533
Ilya <pistol88@gmail.com>
2017-06-14 15:14:05 +0500

    model to callback

--- src/views/order/index.php
+++ src/views/order/index.php
@@ -153,9 +153,9 @@ foreach(Yii::$app->getModule('order')->orderColumns as $column) {
     } elseif(is_array($column) && isset($column['content']) && is_callable($column['content'])) {
         $column = [
             'label' => $column['label'],
-            'value' => function($model) use ($column) {
+            'content' => function($model) use ($column) {
                 $func = $column['content'];
-                return $func();
+                return $func($model);
             }
         ];
     }
